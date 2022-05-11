f1837a855d8872239e6bb8b6bf9d55f39cff7405
kilonzif <faithmueni6@gmail.com>
2022-05-11 09:05:39 +0300

    added initial project files

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+class Config:
+
+  
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+# simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/shopapp_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/shopapp'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}
